2d3574a304196f27b5da8e6746354d884e1a2b9d
Catlin Cummings <catlincummings@catlins-mbp.lan>
2022-04-15 09:37:28 +1200

    Header component made

---
+++ client/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function Header() {
+    return (
+        <div>
+            <h1>Superhero Smackdown Royale!</h1>
+            <h3>Choose your hero</h3>
+        </div>
+    )
+    
+}
+
+export default Header
