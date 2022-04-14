8ffcd25774016c1563d0de9dc10018deb7a09dfc
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2022-04-15 11:17:35 +1200

    Modify components in CSS, create Footer component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,15 +3,22 @@ import React, { useState } from 'react'
 import { getSuperhero } from '../api'
 
 import Header from './Header.jsx'
+import Footer from './Footer'
 
 function App() {
 
   
 
   return (
+    <>
     <div>
       <Header />
     </div>
+    
+    <div>
+      <Footer />
+    </div>
+    </>
   )
 }
 
---
+++ client/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <h1>Bought to you by Team Kit-Kat</h1>
+        </div>
+    )
+    
+}
+
+export default Footer
