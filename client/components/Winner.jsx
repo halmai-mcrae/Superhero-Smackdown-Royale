04c65f233bdbae6aa36937547279c0990e83edee
Kate Blincoe <95556689+kate-blincoe@users.noreply.github.com>
2022-04-15 12:28:09 +1200

    Winner is displaying

--- client/components/Winner.jsx
+++ client/components/Winner.jsx
@@ -1,13 +1,35 @@
 import React, { useState } from 'react'
 
-function Winner () {
+function Winner (props) {
+
+    const heroes = props.heroes
+
+    const displayPowerStats = () => {
+        const statArray = heroes.map(element => element.powerstats )
+        let hero1 = Object.values(statArray[0])
+        let hero2 = Object.values(statArray[1])
+
+        hero1 = hero1.reduce((a, b) => b - a)
+        hero2 = hero2.reduce((a, b) => b - a)
+        
+        if (hero1 > hero2) {
+            console.log(heroes[0].name + ' wins!')
+            setWinner(heroes[0].name)
+        } else {
+            console.log(heroes[1].name + ' wins!')
+            setWinner(heroes[1].name)
+        }
+    }
+
 
     const [winner, setWinner] = useState(false)
 
+
+
     return (
         <div className= 'winner'>
-            <button>Display Winner</button>
-           {/* declare the winner when the stats are compared*/}
+            <button onClick = {displayPowerStats}>Display Winner</button>
+           <p>{winner} Wins!</p>
         </div>
     )
 
